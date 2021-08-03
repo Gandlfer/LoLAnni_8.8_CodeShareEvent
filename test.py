dea970cee25a9a178060ea64f290fcf20bd64020
Gandlfer <darryllee1999@gmail.com>
2021-08-03 13:22:00 +0800

    Image test

---
+++ test.py
@@ -0,0 +1,5 @@
+from python_imagesearch.imagesearch import imagesearch as search
+
+print(search("./captures/InputBox.PNG",0.9))
+print(search("./captures/Button.PNG",0.9))
+print(search("./captures/Confirm.PNG",0.8))
